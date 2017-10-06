edfb7512976fa3252abbef259ef9f95e36104089
Nathan Osman <nathan@quickmediasolutions.com>
2017-10-06 14:14:48 -0700

    Remove account list from home page.

--- server/index.go
+++ server/index.go
@@ -3,17 +3,9 @@ package server
 import (
 	"net/http"
 
-	"github.com/ReformedDevs/anonbot/db"
 	"github.com/flosch/pongo2"
 )
 
 func (s *Server) index(w http.ResponseWriter, r *http.Request) {
-	accounts := []*db.Account{}
-	if err := s.database.C.Find(&accounts).Error; err != nil {
-		http.Error(w, err.Error(), http.StatusInternalServerError)
-		return
-	}
-	s.render(w, r, "index.html", pongo2.Context{
-		"accounts": accounts,
-	})
+	s.render(w, r, "index.html", pongo2.Context{})
 }
