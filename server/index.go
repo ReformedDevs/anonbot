bb90fae952312b66cf4216846d912f93c97bf493
Nathan Osman <nathan@quickmediasolutions.com>
2017-10-07 16:03:17 -0700

    Add statistics.

--- server/index.go
+++ server/index.go
@@ -3,9 +3,37 @@ package server
 import (
 	"net/http"
 
+	"github.com/ReformedDevs/anonbot/db"
 	"github.com/flosch/pongo2"
 )
 
 func (s *Server) index(w http.ResponseWriter, r *http.Request) {
-	s.render(w, r, "index.html", pongo2.Context{})
+	var (
+		numUsers       int64
+		numAccounts    int64
+		numSuggestions int64
+		numQueuedItems int64
+	)
+	if err := s.database.C.Model(&db.User{}).Count(&numUsers).Error; err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
+	if err := s.database.C.Model(&db.Account{}).Count(&numAccounts).Error; err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
+	if err := s.database.C.Model(&db.Suggestion{}).Count(&numSuggestions).Error; err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
+	if err := s.database.C.Model(&db.QueueItem{}).Count(&numQueuedItems).Error; err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
+	s.render(w, r, "index.html", pongo2.Context{
+		"num_users":        numUsers,
+		"num_accounts":     numAccounts,
+		"num_suggestions":  numSuggestions,
+		"num_queued_items": numQueuedItems,
+	})
 }
