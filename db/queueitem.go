c995b0e3a451d79667236bc61f879d5644ffbc0e
Nathan Osman <nathan@quickmediasolutions.com>
2017-10-07 17:39:57 -0700

    Add MediaURL field to suggestions and queued items.

--- db/queueitem.go
+++ db/queueitem.go
@@ -10,6 +10,7 @@ type QueueItem struct {
 	ID        int64
 	Date      time.Time
 	Text      string   `gorm:"not null"`
+	MediaURL  string   `gorm:"not null"`
 	User      *User    `gorm:"ForeignKey:UserID"`
 	UserID    int64    `sql:"type:int REFERENCES users(id) ON DELETE CASCADE"`
 	Account   *Account `gorm:"ForeignKey:AccountID"`
