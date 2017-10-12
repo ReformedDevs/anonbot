3a3de2759570126ce7d3895f0786d08549094cc0
Nathan Osman <nathan@quickmediasolutions.com>
2017-10-11 20:11:38 -0700

    Add file missing from last commit (oops).

---
+++ db/tweet.go
@@ -0,0 +1,16 @@
+package db
+
+import (
+	"time"
+)
+
+// Tweet represents a queued item that has since been tweeted.
+type Tweet struct {
+	ID       int64
+	TweetID  int64
+	Date     time.Time
+	Text     string `gorm:"not null"`
+	MediaURL string `gorm:"not null"`
+	User     *User  `gorm:"ForeignKey:UserID"`
+	UserID   int64  `sql:"type:int REFERENCES users(id) ON DELETE CASCADE"`
+}
